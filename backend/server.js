5fbce51530d007a0919201205402f6d34135a818
MUSA SALUMU MUHOZYA <musa24@MacBook-Pro-MUSA.local>
2020-10-10 04:58:59 +0300

    setup ES module,dotenv,concurrently

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
-const express = require('express');
-const products = require('./data/products');
+import express from 'express';
+import products from './data/products.js';
+import dotenv from 'dotenv';
 
 const app = express();
+
+dotenv.config();
 
 //Route
 app.get('/', (req, res) => {
@@ -17,7 +20,8 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product);
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+const mode = process.env.NODE_ENV;
 app.listen(PORT, () => {
-  console.log(`The server is running on port ${PORT}`);
+  console.log(`The server is running on port ${PORT}  in ${mode} mode `);
 });
