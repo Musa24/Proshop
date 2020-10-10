b8f22f5bae336ecf510af1f8ee60541cf5a46eed
MUSA SALUMU MUHOZYA <musa24@MacBook-Pro-MUSA.local>
2020-10-10 03:36:57 +0300

    ProductScreen component

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,71 @@
 import React from 'react';
+import { Button, Card, Col, Image, ListGroup, Row } from 'react-bootstrap';
+import { Link, useParams } from 'react-router-dom';
+import Rating from '../components/Rating';
+import products from '../products';
 
 function ProductScreen() {
-  return <div>Products</div>;
+  const { id } = useParams('id');
+  const product = products.find((product) => product._id === id);
+  const {
+    image,
+    name,
+    rating,
+    numReviews,
+    price,
+    description,
+    countInStock,
+  } = product;
+  return (
+    <>
+      <Link className="btn btn-light" to="/">
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={image} alt={name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h4>{name}</h4>
+              <Rating value={rating} text={`${numReviews} reviews`} />
+            </ListGroup.Item>
+            <ListGroup.Item>Price: ${price}</ListGroup.Item>
+            <ListGroup.Item>Description: {description} </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>${price}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>{countInStock > 0 ? 'In Stock' : 'Out of Stock'}</Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Button
+                  className="btn btn-block"
+                  type="button"
+                  disabled={countInStock === 0}
+                >
+                  add to Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
 }
 
 export default ProductScreen;
