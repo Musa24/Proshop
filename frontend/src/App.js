01b25fb737a4385192ef67703456528b01dd0e7e
MUSA SALUMU MUHOZYA <musa24@MacBook-Pro-MUSA.local>
2020-10-10 03:14:10 +0300

    Setup react-router-bootstrap &  react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { Container } from 'react-bootstrap';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import HomeScreen from './screens/HomeScreen';
+import { Route } from 'react-router-dom';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
@@ -10,7 +12,10 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route exact path="/" component={HomeScreen} />
+          <Route path="/product/:id" exact>
+            <ProductScreen />
+          </Route>
         </Container>
       </main>
       <Footer />
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 import Rating from './Rating';
+import { Link } from 'react-router-dom';
 
 function Product({ product }) {
   const { _id, image, name, rating, numReviews, price } = product;
   return (
     <Card className="my-3 p-3 rounded">
-      <a href={`/product/${_id}`}>
+      <Link to={`/product/${_id}`}>
         <Card.Img src={image} variant="top" />
-      </a>
+      </Link>
 
       <Card.Body>
-        <a href={`/product/${_id}`}>
+        <Link to={`/product/${_id}`}>
           <Card.Title as="div">
             <strong>{name}</strong>
           </Card.Title>
-        </a>
+        </Link>
         <Card.Text as="div">
           <Rating value={rating} text={`${numReviews} reviews`} />
         </Card.Text>
