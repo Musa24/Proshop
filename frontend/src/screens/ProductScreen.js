0380f8daf7449cfb4a0f4cf8e5ea815cb0aa9a60
MUSA SALUMU MUHOZYA <musa24@MacBook-Pro-MUSA.local>
2020-10-12 02:54:06 +0300

    ProductDetails  actions & reducers

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,21 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Button, Card, Col, Image, ListGroup, Row } from 'react-bootstrap';
 import { Link, useParams } from 'react-router-dom';
 import Rating from '../components/Rating';
-import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProductDetails } from '../actions/productActions';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 
 function ProductScreen() {
-  const [product, setProduct] = useState({});
+  const dispatch = useDispatch();
   const { id } = useParams('id');
+  const productListDetails = useSelector((state) => state.productListDetails);
+  const { loading, error, product } = productListDetails;
 
   useEffect(() => {
-    const fetchProduct = async () => {
-      const { data } = await axios.get(`/api/products/${id}`);
-      setProduct(data);
-    };
-
-    fetchProduct();
-  }, [id]);
+    dispatch(listProductDetails(id));
+  }, [dispatch, id]);
 
   const {
     image,
@@ -31,49 +31,55 @@ function ProductScreen() {
       <Link className="btn btn-light" to="/">
         Go Back
       </Link>
-      <Row>
-        <Col md={6}>
-          <Image src={image} alt={name} fluid />
-        </Col>
-        <Col md={3}>
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <h4>{name}</h4>
-              <Rating value={rating} text={`${numReviews} reviews`} />
-            </ListGroup.Item>
-            <ListGroup.Item>Price: ${price}</ListGroup.Item>
-            <ListGroup.Item>Description: {description} </ListGroup.Item>
-          </ListGroup>
-        </Col>
-        <Col md={3}>
-          <Card>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Row>
+          <Col md={6}>
+            <Image src={image} alt={name} fluid />
+          </Col>
+          <Col md={3}>
             <ListGroup variant="flush">
               <ListGroup.Item>
-                <Row>
-                  <Col>Price:</Col>
-                  <Col>${price}</Col>
-                </Row>
+                <h4>{name}</h4>
+                <Rating value={rating} text={`${numReviews} reviews`} />
               </ListGroup.Item>
-              <ListGroup.Item>
-                <Row>
-                  <Col>Status:</Col>
-                  <Col>{countInStock > 0 ? 'In Stock' : 'Out of Stock'}</Col>
-                </Row>
-              </ListGroup.Item>
-
-              <ListGroup.Item>
-                <Button
-                  className="btn btn-block"
-                  type="button"
-                  disabled={countInStock === 0}
-                >
-                  add to Cart
-                </Button>
-              </ListGroup.Item>
+              <ListGroup.Item>Price: ${price}</ListGroup.Item>
+              <ListGroup.Item>Description: {description} </ListGroup.Item>
             </ListGroup>
-          </Card>
-        </Col>
-      </Row>
+          </Col>
+          <Col md={3}>
+            <Card>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>${price}</Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status:</Col>
+                    <Col>{countInStock > 0 ? 'In Stock' : 'Out of Stock'}</Col>
+                  </Row>
+                </ListGroup.Item>
+
+                <ListGroup.Item>
+                  <Button
+                    className="btn btn-block"
+                    type="button"
+                    disabled={countInStock === 0}
+                  >
+                    add to Cart
+                  </Button>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      )}
     </>
   );
 }
