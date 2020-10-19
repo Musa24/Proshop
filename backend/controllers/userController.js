ea09a25a8d7303265aedc617daec57353f930365
MUSA SALUMU MUHOZYA <musa24@MacBook-Pro-MUSA.local>
2020-10-19 23:40:33 +0300

    Register a User

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,4 +24,51 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+//@Desc   Get User Profile
+//@route GET /api/users/profile
+// @access private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  const { _id, email, name, isAdmin } = user;
+  if (user) {
+    res.json({ _id, name, email, isAdmin });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+//@Desc   Register User
+//@route POST /api/users
+// @access public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  console.log(name, email, password);
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
+
+export { authUser, getUserProfile, registerUser };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,14 @@
 import express from 'express';
 const router = express.Router();
-import { authUser } from '../controllers/userController.js';
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+} from '../controllers/userController.js';
+import { protectRoute } from '../middleware/authMiddleware.js';
 
 router.post('/login', authUser);
+router.route('/profile').get(protectRoute, getUserProfile);
+router.post('/', registerUser);
 
 export default router;
