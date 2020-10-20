047e58e2c4236eae257902cb0bfab2c5f28a1787
MUSA SALUMU MUHOZYA <musa24@MacBook-Pro-MUSA.local>
2020-10-20 05:43:32 +0300

    Show loggedIn  user && logout functionality

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,6 +3,7 @@ import {
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
 } from '../constants/userConstants';
 
 export const login = (email, password) => async (dispatch) => {
@@ -37,4 +38,12 @@ export const login = (email, password) => async (dispatch) => {
           : error.message,
     });
   }
+};
+
+export const logout = () => (dispatch) => {
+  //remove user from local Storage
+  localStorage.removeItem('userInfo');
+  dispatch({
+    type: USER_LOGOUT,
+  });
 };
