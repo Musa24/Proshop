2c6ed2aabd02b076bcad30fac1b7832b83050911
MUSA SALUMU MUHOZYA <musa24@MacBook-Pro-MUSA.local>
2020-11-05 00:07:51 +0300

    ProductList Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
 import UserEditScreen from './screens/UserEditScreen';
+import ProductListScreen from './screens/ProductListScreen';
 
 function App() {
   return (
@@ -38,6 +39,11 @@ function App() {
           <Route exact path="/users/:profile" component={ProfileScreen} />
           <Route exact path="/admin/userlist" component={UserListScreen} />
           <Route exact path="/admin/user/:id/edit" component={UserEditScreen} />
+          <Route
+            exact
+            path="/admin/productlist"
+            component={ProductListScreen}
+          />
         </Container>
       </main>
       <Footer />
