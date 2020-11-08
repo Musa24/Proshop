4e8766d76e21993599f3ba25d4734d56db51b26f
MUSA SALUMU MUHOZYA <musa24@MacBook-Pro-MUSA.local>
2020-11-08 12:26:24 +0300

    Imge Upload conf with multer

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes';
 
 const app = express();
 
@@ -26,10 +28,15 @@ app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
+//Static files
+const __dirname = path.resolve(); // For es6 syntax ( import)
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
 // routes middleware
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
 //ERROR middleware
 app.use(notFound);
